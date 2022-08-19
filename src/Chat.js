d35a7aee9fcd17bbd93596c8661954b43d75e393
Ashishdev221 <84578874+Ashishdev221@users.noreply.github.com>
2022-08-19 20:03:27 +0530

    frontend complete

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,52 @@
+import { Avatar, IconButton } from '@mui/material';
 import React from 'react';
 import "./Chat.css";
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import MicIcon from '@mui/icons-material/Mic';
 function Chat() {
   return (
     <div className='chat'>
-        <h1>i am chat</h1>
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className='chat__message'>
+          <span className='chat__name'>Sonny</span>
+          This is a message
+          <span className='chat__timestamp'>{new Date().toUTCString()}</span>
+        </p>
+        <p className='chat__message chat__reciever'>
+          <span className='chat__name'>Sonny</span>
+          This is a message
+          <span className='chat__timestamp'>{new Date().toUTCString()}</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input type="text" placeholder='type a message' />
+          <button type='submit'>Send a message</button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   )
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,25 +4,39 @@ import ChatIcon from '@mui/icons-material/Chat';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Avatar, IconButton } from '@mui/material';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import SidebarChat from './SidebarChat';
 function Sidebar() {
-  return (
-    <div className='sidebar'>
-        <div className="sidebar__header">
-        <Avatar src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' />
-            <div className="sidebar__headerRight">
-                <IconButton>
-                    <DonutLargeIcon />
-                </IconButton>
-                <IconButton>
-                    <ChatIcon />
-                </IconButton>
-                <IconButton>
-                    <MoreVertIcon />
-                </IconButton>
+    return (
+        <div className='sidebar'>
+            <div className="sidebar__header">
+                <Avatar src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlinedIcon />
+                    <input type="text" placeholder='Search or start new chat' />
+                </div>
+            </div>
+            <div className="sidebar__chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Sidebar
